697d044193ab1eb78b697c0baf1f4eb5f6223fbb
sh1nysparkly <118570543+sh1nysparkly@users.noreply.github.com>
2024-04-08 09:44:41 -0600

    Update ssd.py

--- ssd.py
+++ ssd.py
@@ -8,9 +8,15 @@ import numpy as np
 import json
 from google.oauth2 import service_account 
 
-# Load credentials from environment variable set by Streamlit Cloud
-credentials_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+credentials_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS") 
+
+# Temporary Debugging
+print("---- Credentials from environment ----")
+print(credentials_json)  
+print("---- End credentials ----")
+
 credentials = service_account.Credentials.from_service_account_info(json.loads(credentials_json))
+
 
 
 def analyze_text_salience(text):
